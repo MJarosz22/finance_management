302712ca93cf2a5619709be8941c8e61b7fe39d4
Marcin Jarosz <m.w.jarosz@student.tudelft.nl>
2022-08-09 14:23:47 +0200

    added support for the second bank account, currency conversion

--- main.py
+++ main.py
@@ -1,37 +1,78 @@
+import requests
 import csv
 import gspread
 import time
 
 MONTH = 'july'
 
-file = f"mbank-pln-{MONTH}.csv"
+
+class RealTimeCurrencyConverter():
+    def __init__(self,url):
+        self.data= requests.get(url).json()
+        self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount):
+        if from_currency != 'USD':
+            amount = amount / self.currencies[from_currency]
+        amount = round(amount * self.currencies[to_currency], 2)
+        return amount
+
+url = 'https://api.exchangerate-api.com/v4/latest/USD'
+converter = RealTimeCurrencyConverter(url)
+
+mbankFile = f"mbank-{MONTH}.csv"
+abnFile = f"abn-{MONTH}.csv"
 
 
 def mBankFin(file):
     transactions = []
-    sum = 0
     with open(file, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ';')
+        csv_reader = csv.reader(csv_file, delimiter=';')
         for row in csv_reader:
-            date = row[0] #date
-            name = row[1].strip() #description
-            #print(row[2]) #account
-            #print(row[3]) #category
-            amount = float(row[4].split(' ')[0].replace(',', '.')) #amount
-            sum += amount
+            date = row[0]
+            name = row[1].strip()
+            amount = float(row[4].split(' ')[0].replace(',', '.'))
             currency = row[4].split(' ')[1]
+            if currency != 'EUR':
+                amount = converter.convert(currency, 'EUR', amount)
+                currency == 'EUR'
             category = 'other'
             transaction = (date, name, amount, currency, category)
             transactions.append(transaction)
+            print(transaction)
         return transactions
+
+
+def abnFin(file):
+    transactions = []
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        for row in csv_reader:
+            currency = row[1]
+            date = row[2][0:4] + '-' + row[2][4:6] + '-' + row[2][6:8]
+            amount = float(row[6].replace(',', '.'))
+            name = row[8]
+            for x in range(9, len(row)):
+                name += ' ' + row[x]
+            category = 'other'
+            transaction = (date, name, amount, currency, category)
+            transactions.append(transaction)
+            print(transaction)
+    return transactions
+
 
 sa = gspread.service_account()
 sh = sa.open("Personal Finances")
 
 wks = sh.worksheet(f"{MONTH}")
 
-rows = mBankFin(file)
+rowsMbank = mBankFin(mbankFile)
+rowsAbn = abnFin(abnFile)
+abnFin(abnFile)
+for row in rowsMbank:
+    wks.insert_row([row[0], row[1], row[4], row[2]], 8)
+    time.sleep(2)
 
-for row in rows:
-    wks.insert_row([row[0], row[1], row[3], row[2]], 8)
+for row in rowsAbn:
+    wks.insert_row([row[0], row[1], row[4], row[2]], 8)
     time.sleep(2)
